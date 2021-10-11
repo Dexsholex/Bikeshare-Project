9f23724e69ee1587239ad0ad2d0da54a472f0f21
Oyinloye Oluwasola Abdullahi <Abdullahioyinloye1115@gmail.com>
2021-10-11 14:17:59 +0100

    Update document strings and line comments

--- Bikeshare.py
+++ Bikeshare.py
@@ -11,9 +11,9 @@ def get_filters():
     Asks user to specify a city, month, and day to analyze.
 
     Returns:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        (str) city - name of the city to analyze.
+        (str) month - name of the month to filter by, or "all" to apply no month filter.
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter.
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     print('Welcome to the Program')
@@ -34,7 +34,7 @@ def get_filters():
         print('You have chosen {}'.format(city.title()))
         break
 
-    # TO DO: get user input for month (all, january, february, ... , june)
+    # TO DO: get user input for month (all, january, february, ... june)
     MONTH_DATA = {'january': 1, 'february': 2, 'march': 3,
                   'april': 4, 'may': 5, 'june': 6, 'all': 7}
     month = ''
